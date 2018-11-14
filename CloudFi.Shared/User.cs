0a5c7dc22d9b8fbccb0930f789e00541d030c2aa
Nick Anderson <njcanderson@gmail.com>
2018-11-13 21:14:41 -0500

    starting to use ef core and models

---
+++ CloudFi.Shared/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CloudFi.Shared
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public virtual List<WifiPassword> WifiPasswords { get; set; }
+    }
+}
---
+++ CloudFi.Shared/WifiPassword.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudFi.Shared
+{
+    public class WifiPassword
+    {
+        public int OwnerUserId { get; set; }
+        public string Ssid { get; set; }
+        public string Password { get; set; }
+
+        public virtual User Owner { get; set; }
+    }
+}
