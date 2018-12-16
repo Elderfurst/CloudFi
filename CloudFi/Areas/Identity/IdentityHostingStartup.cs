4687591e61b1a6d2b19afc855ebc5117ee582ef0
Nick Anderson <njcanderson@gmail.com>
2018-12-16 10:41:28 -0500

    adding scaffolded identity files

---
+++ CloudFi/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(CloudFi.Areas.Identity.IdentityHostingStartup))]
+namespace CloudFi.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => { });
+        }
+    }
+}
